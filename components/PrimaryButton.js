7fc788054e7662d9d249fa3ccd3b158fcd8f7449
Kosemer <82768146+Kosemer@users.noreply.github.com>
2025-04-02 21:13:04 +0200

    PrimaryButton

---
+++ components/PrimaryButton.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+function PrimaryButton(props) {
+    return  <View>
+        <Text>{props.children}</Text>
+    </View>
+}
+
+export default PrimaryButton;
